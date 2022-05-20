4dae45d65f10fa0515ab6b256f1921e2c235bfa9
ausiasxp <al397425@uji.es>
2022-05-20 03:16:32 +0200

    treshhold and random arreglado

--- js/play.js
+++ js/play.js
@@ -135,6 +135,7 @@ function updateCounter(){
 }
 
 function createEnemiesPrueba(i){
+    random = Math.floor(Math.random() * 27);
     let enemy = game.add.sprite(100 * i, 200, 'ufo');
     game.physics.enable(enemy, Phaser.Physics.ARCADE);
     enemy.enableBody = true;
@@ -147,10 +148,10 @@ function createEnemiesPrueba(i){
     console.log("creado enemies["+i+"] es "+enemies[i]);
 
     console.log(word[w1]+" textword");
-    textwords[i] = (word[w1][i]);
+    textwords[i] = (word[w1][random]);
     
     console.log("textword antes de texto temporal "+ textwords[i]);
-    textoTemporal[i] = game.add.text(enemy.x, enemy.y, textwords[i], styleI);
+    textoTemporal[i] = game.add.text(enemy.x+treshold, enemy.y+treshold, textwords[i], styleI);
     //textoTemporal.anchor.set(0.5);
     console.log("texto temporal["+i+"] es "+textoTemporal[i].text);
     console.log("words 0 es "+words[0]);
@@ -203,8 +204,8 @@ function moveText() {
 
     
     for (var i = 0; i <= currentEnemiesNum-1; i++) {
-        words[i].x = Math.floor(enemies[i].body.x);
-        words[i].y = Math.floor(enemies[i].body.y);
+        words[i].x = Math.floor(enemies[i].body.x+treshold);
+        words[i].y = Math.floor(enemies[i].body.y+treshold);
 
     }
 
